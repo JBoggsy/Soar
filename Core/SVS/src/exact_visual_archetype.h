93764332c72e8e4823ed795b643faa755774e2ed
James Boggs <boggsj@umich.edu>
2020-10-12 14:12:04 -0400

    Refactor visual archetypes to use class templating.

--- Core/SVS/src/exact_visual_archetype.h
+++ Core/SVS/src/exact_visual_archetype.h
@@ -8,20 +8,64 @@
 /**
  * @brief Represents visual knowledge exactly as it receives it.
  * 
- * @details The template parameter indicates which subclass of image_base the
- * percept is stored as.
+ * @details A visual_archetype instance holds visual knowledge related to a 
+ * particular kind of entity or a specific individual entity. The former we call
+ * "entity types" and the latter "entity tokens". Examples of entity types 
+ * include "person", "building", and "word". Examples of entity include "John 
+ * Laird", "BBB Building", and "soar".
+ * 
+ * The exact_visual_archetype class represents visual percepts exactly as they
+ * are received, and does matching as a true/false comparison of equality. Thus
+ * it lacks any amount of flexibility or subtlety and should not be used for
+ * any real-world application.
+ * 
+ * exact_visual_archetype is templated to allow specializing for different types
+ * of visual input, including the openv_image and pcl_image classes.
+ * 
+ * @sa visual_memory
+ * @sa image_base
+ * @sa opencv_image
+ * @sa pcl_image
  */
-template<typename image_type>
+template<typename img_t>
 class exact_visual_archetype : public visual_archetype {
     protected:
-        image_type* _percept;
+        img_t _percept;
     
     public:
         exact_visual_archetype(std::string entity_id);
 
-        void store_percept(image_type* example);
-        float compare(image_type* percept);
-        void reconstruct(image_type* output);
+        /**
+         * @brief Store a new percept in the archetype.
+         * 
+         * @details Given a new visual percept, integrate it into the 
+         * visual_archetype instance by cloning the percept data into the
+         * _percept field of the archetype instance.
+         * 
+         * @param percept The new visual percept to be integrated.
+         */
+        void store_percept(img_t example) override;
+
+        /**
+         * @brief Indicate whether the given percept is the same as this one.
+         * 
+         * @details Given a visual percept, return either 0.0 to indicate 
+         * inequality or 1.0 to indicate equaity. 
+         * 
+         * @param percept The visual percept which should be compared against.
+         */
+        float compare(img_t percept) override;
+
+        /**
+         * @brief Generate a mental image of the entity this archetype represents.
+         * 
+         * @details Clone the percept which is stored in the _percept field of
+         * this archetype into the output.
+         * 
+         * @param output Pointer to an instance of the correct image type img_t
+         * which the reconstruction will be stored in.
+         */
+        void reconstruct(img_t* output) override;
 };
 
 #endif
