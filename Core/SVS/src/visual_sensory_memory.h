30fdb381148ddc5ba6890d83671b18905e7ec504
James Boggs <boggsj@umich.edu>
2021-03-17 17:40:48 -0400

    Create preliminary visual_sensory_memory class

---
+++ Core/SVS/src/visual_sensory_memory.h
@@ -0,0 +1,29 @@
+#ifndef VISUAL_SENSORY_MEMORY_H
+#define VISUAL_SENSORY_MEMORY_H
+
+//////////////
+// PREAMBLE //
+//////////////
+// SVS includes
+#include "image.h"
+// forward definitions
+class svs;
+
+class visual_sensory_memory
+{
+private:
+    svs* _svs_ptr;
+
+    const static int PERCEPT_BUFFER_SIZE = 1;
+    opencv_image percept_buffer [PERCEPT_BUFFER_SIZE];
+
+public:
+    visual_sensory_memory(svs* svs_ptr);
+    ~visual_sensory_memory();
+
+    void update_percept_buffer(opencv_image new_percept);
+    
+};
+
+
+#endif
