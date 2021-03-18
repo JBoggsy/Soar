6432c7aedc414a2f972dc34692499627eb6f46be
James Boggs <boggsj@umich.edu>
2021-03-18 12:47:54 -0400

    Add draw_percept_buffer method to visual_sensory_memory

--- Core/SVS/src/visual_sensory_memory.h
+++ Core/SVS/src/visual_sensory_memory.h
@@ -15,14 +15,14 @@ private:
     svs* _svs_ptr;
 
     const static int PERCEPT_BUFFER_SIZE = 1;
-    opencv_image percept_buffer [PERCEPT_BUFFER_SIZE];
+    opencv_image* percept_buffer [PERCEPT_BUFFER_SIZE];
 
 public:
     visual_sensory_memory(svs* svs_ptr);
     ~visual_sensory_memory();
 
-    void update_percept_buffer(opencv_image new_percept);
-    
+    void update_percept_buffer(const cv::Mat& new_percept);
+    void draw_percept_buffer();
 };
 
 
