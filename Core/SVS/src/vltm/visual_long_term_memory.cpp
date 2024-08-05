49021195e2e647cd17429498a0305ff85211ed75
James Boggs <jmsboggs@gmail.com>
2024-08-05 17:45:54 -0400

    Create VLTM CLI interfaces for learning and generating VCDs

--- Core/SVS/src/vltm/visual_long_term_memory.cpp
+++ Core/SVS/src/vltm/visual_long_term_memory.cpp
@@ -4,6 +4,8 @@
 #include <list>
 #include <unordered_map>
 #include <exception>
+// Third-party Includes
+#include "base64.h"
 // SVS Includes
 #include "image.h"
 #include "visual_long_term_memory.h"
@@ -59,13 +61,44 @@ void visual_long_term_memory<img_T, atype_T>::search(img_T* percept, float thres
 
 template <typename img_T, template<typename T> class atype_T>
 void visual_long_term_memory<img_T, atype_T>::proxy_get_children(std::map<std::string, cliproxy*>& c) {
+    c["list"] = new memfunc_proxy<visual_long_term_memory<img_T, atype_T>>(this, &visual_long_term_memory<img_T, atype_T>::cli_list_vcd_ids);
+
+    c["learn"] = new memfunc_proxy<visual_long_term_memory<img_T, atype_T>>(this, &visual_long_term_memory<img_T, atype_T>::cli_learn);
+    c["learn"]->add_arg("CLASS_NAME", "The name of the class (aka VCD ID) to create/update.");
+    c["learn"]->add_arg("IMG_DATA", "The base64-encoded data of the image to update the VCD with.");
+
+    c["generate"] = new memfunc_proxy<visual_long_term_memory<img_T, atype_T>>(this, &visual_long_term_memory<img_T, atype_T>::cli_generate);
+    c["generate"]->add_arg("CLASS_NAME", "The name of the class (aka VCD ID) to generate an image of.");
 }
 
 template <typename img_T, template<typename T> class atype_T>
 void visual_long_term_memory<img_T, atype_T>::proxy_use_sub(const std::vector<std::string>& args, std::ostream& os) {
     os << "========== VLTM INTERFACE ==========" << std::endl;
+    os << "VCDs: " << (int)_archetypes.size() << std::endl;
     os << "CLI USAGE:" << std::endl << std::endl;
+    os << "svs vltm - Prints this message." << std::endl;
+    os << "svs vltm.list - Prints a newline-separated list of the VCD IDs in VLTM." << std::endl;
+    os << "svs vltm.learn <CLASS_NAME> <IMG_DATA> - Uses the image represented in <IMAGE_DATA> to update (or create) the VCD with the given class name. <IMAGE_DATA> should be a base64 encoding of the image." << std::endl;
+    os << "svs vltm.generate <CLASS_NAME> - Generates an image of the VCD with the given class name, outputting it in base64. Empty string means no VCD with that class name exists." << std::endl;
     os << "====================================" << std::endl;
+}
+
+template <typename img_T, template<typename T> class atype_T>
+void visual_long_term_memory<img_T, atype_T>::cli_list_vcd_ids(const std::vector<std::string>& args, std::ostream& os) {
+
+}
+
+template <typename img_T, template<typename T> class atype_T>
+void visual_long_term_memory<img_T, atype_T>::cli_learn(const std::vector<std::string>& args, std::ostream& os) {
+    os << "Need to use specialized vltm." << std::endl;
+    return;
+}
+
+template <typename img_T, template<typename T> class atype_T>
+void visual_long_term_memory<img_T, atype_T>::cli_generate(const std::vector<std::string>& args, std::ostream& os) {
+    os << "Need to use specialized vltm." << std::endl;
+    return;
+
 }
 
 #ifdef ENABLE_OPENCV
@@ -109,6 +142,44 @@ void visual_long_term_memory<opencv_image, exact_visual_concept_descriptor>::mat
         output[i]->confidence = match_confidence;
         best_matches.pop_front();
     }
+}
+
+template <>
+void visual_long_term_memory<opencv_image, exact_visual_concept_descriptor>::cli_learn(const std::vector<std::string>& args, std::ostream& os) {
+    if (args.size() < 2) {
+        os << "Must specify class name and image data." << std::endl;
+        return;
+    }
+
+    std::string vcd_id(args[0]);
+    std::string img_data(args[1]);
+    std::string decoded_data = base64_decode(img_data);
+    std::vector<uchar> data(decoded_data.begin(), decoded_data.end());
+    cv::Mat raw_img = cv::imdecode(cv::Mat(data), -1);
+    opencv_image* image = new opencv_image();
+    image->update_image(raw_img);
+
+    store_percept(image, vcd_id);
+}
+
+template <>
+void visual_long_term_memory<opencv_image, exact_visual_concept_descriptor>::cli_generate(const std::vector<std::string>& args, std::ostream& os) {
+    if (args.empty()) {
+        os << "No vib name specified." << std::endl;
+        return;
+    }
+
+    std::string vcd_id(args[0]);
+
+    opencv_image* gened_image = new opencv_image();
+    recall(vcd_id, gened_image);
+
+
+    std::vector<uchar> raw_png_data;
+    std::string b64_data;
+    cv::imencode(std::string(".png"), *(gened_image->get_image()), raw_png_data);
+    b64_data = base64_encode(raw_png_data.data(), raw_png_data.size());
+    os << b64_data << std::endl;
 }
 
 template class visual_long_term_memory<opencv_image, exact_visual_concept_descriptor>;
